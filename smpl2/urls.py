4c373367b39d94e40b59382b643eb1a65f5d1b74
Akshayajayarajan <akshayaav722@gmail.com>
2022-09-21 20:05:08 +0530

    second commit

--- smpl2/urls.py
+++ smpl2/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from smpl2app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.loadfirst,name='loadfirst'),
+    path('loadsecond',views.loadsecond,name='loadsecond')
+
 ]
